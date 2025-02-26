e7707b1833a590b89cfc9e878ceb62132273b8bd
MakkoDakko <93068937+MakkoDakko@users.noreply.github.com>
2025-02-26 14:39:03 +0700

    Create whatever.php

---
+++ whatever.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "Hôm nay là ngày " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+?>
+
+</body>
+</html>
